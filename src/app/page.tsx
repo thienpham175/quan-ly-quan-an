fa4db2e2b00088f14f20ee4bf630f9120e0cf6b4
thienpham175 <thienpdc@where.vn>
2024-10-04 14:08:45 +0700

    Khởi tạo tạo dự án Nextjs vs ShadcnUi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -22,6 +23,8 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+
+        <Button>Click me</Button>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
